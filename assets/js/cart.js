705f636d88b72689096b909fa8f46c6a56a77cfb
vbhargav13 <100362616+vbhargav13@users.noreply.github.com>
2025-04-19 10:10:24 +0530

    Add files via upload

---
+++ assets/js/cart.js
@@ -0,0 +1,7 @@
+
+function addToCart(productName, price) {
+  let cart = JSON.parse(localStorage.getItem('cart')) || [];
+  cart.push({ name: productName, price: price });
+  localStorage.setItem('cart', JSON.stringify(cart));
+  alert(productName + " added to cart!");
+}
